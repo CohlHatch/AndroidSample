7c3d742da02297288f700a9d9ca001142cd0e050
CohlHatch <60143723+CohlHatch@users.noreply.github.com>
2020-02-26 21:22:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.androidsample
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    fun convertCurrency(view: View) {
+        if (dollar_hint.text.isNotEmpty()) {
+
+            val dollarValue = dollar_hint.text.toString().toFloat()
+
+            val euroValue = dollarValue * 0.85f
+
+            textView.text = euroValue.toString()
+        } else {
+            textView.text = getString(R.string.no_value_string)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
