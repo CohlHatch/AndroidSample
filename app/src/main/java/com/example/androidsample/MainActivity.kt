91d2c7d0075553a8ea3a481b5eca3266837a9efb
CoachLemm <lemm@jc.edu>
2020-02-28 11:57:37 -0600

    UPDATE FINAL

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -31,7 +31,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
         }
     }
 
